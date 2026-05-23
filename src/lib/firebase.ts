e0916538dbae39b968e5c9dbb72c83842030ad21
rishabh3635 <rishabhc784@gmail.com>
2026-05-23 11:40:21 +0530

    feat: add Firebase Functions import to enable cloud functions integration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAuth, type Auth } from "firebase/auth";
+import { getFunctions, type Functions } from "firebase/functions";
 
 const config = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
